76aeb6af7d6db25118314744d4de2ae39a833e9c
Vitaliy Sekan <v.siekan@svitla.com>
2017-08-21 21:04:55 +0300

    changed logic of root and vendor folder detection

--- scripts/git-hooks/GitHooks.php
+++ scripts/git-hooks/GitHooks.php
@@ -11,25 +11,15 @@ use Composer\Script\Event;
 use Symfony\Component\Filesystem\Filesystem;
 
 class GitHooks {
-
-  protected static function getProjectRoot() {
-    return dirname(__FILE__);
-  }
-
-  protected static function getGitHooksOriginDirectory() {
-    $project_root = static::getProjectRoot();
-    return $project_root . '/vendor/jover_be/drupal-code-check/git-hooks';
-  }
-
-  protected static function getGitHooksTargetDirectory() {
-    $project_root = static::getProjectRoot();
-    return $project_root . '/.git/hooks';
-  }
-
-  public static function create(Event $event) {
+  
+  public static function create(\Composer\Script\Event $event) {
     $fs = new Filesystem();
-    $origin_dir = static::getGitHooksOriginDirectory();
-    $target_dir = static::getGitHooksTargetDirectory();
+
+    $composer = $event->getComposer();
+    $vendorPath = $composer->getConfig()->get('vendor-dir');
+
+    $origin_dir = $vendorPath . '/jover_be/drupal-code-check/git-hooks';
+    $target_dir = getcwd() . '/.git/hooks';
 
     $git_hooks = [
       'pre-commit',
@@ -39,6 +29,9 @@ class GitHooks {
     foreach ($git_hooks as $git_hook) {
       $origin_file = $origin_dir . '/' . $git_hook;
       $target_file = $target_dir . '/' . $git_hook;
+
+      echo 'Origin file: ' . $origin_file . "\n";
+      echo 'Target file:' . $target_file . "\n";
 
       if ($fs->exists($origin_file)) {
         // Symlink the target to origin (force copy on Windows).
